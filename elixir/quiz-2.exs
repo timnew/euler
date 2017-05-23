0e6c440540940f4fd385f74fd8cc3f52583e64f9
DD <timnew.wti@gmail.com>
2017-05-23 11:53:16 +1000

    Replace filter with take_while

--- elixir/quiz-2.exs
+++ elixir/quiz-2.exs
@@ -3,7 +3,7 @@ require Integer
 fabonacci  = Stream.unfold({1, 1}, fn {x, y} -> {x, {y, x + y}} end)
 
 fabonacci
-|> Stream.filter(fn x -> x < 4_000_000 end)
+|> Stream.take_while(fn x -> x < 4_000_000 end)
 |> Stream.map(fn x -> IO.puts(x); x end)
 |> Stream.filter(&Integer.is_even(&1))
 |> Stream.scan(0, &(&1 + &2))
